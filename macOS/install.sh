eece597bcfa297b1e0f726e981aceb4613a5b6b8
Lauren Shultz <lshultz2020@gmail.com>
2019-08-23 18:37:17 -0500

    base mac install script

---
+++ macOS/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+accepted=0
+echo "
+What version enviroment version would you like to setup?
+    [0] vBasic
+    [1] vGraham
+    [2] vFun
+    [3] Quit"
+
+read -p "Selection: " version
+
+while [  $accepted -eq 0 ]; do
+    if [ $version -eq 0 ]
+    then
+        accepted=1
+#        ./vBasic.sh
+        "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vBasic.sh)\""
+    elif [ $version -eq 1 ]
+    then
+        accepted=1
+#        ./vGraham.sh
+        "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vGraham.sh)\""
+    elif [ $version -eq 2 ]
+    then
+        accepted=1
+#       ./vFun.sh
+        "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vFun.sh)\""
+    elif [ $version -eq 3 ]
+    then
+        accepted=1
+        echo "Goodbye!"
+    else
+        read -p "Invalid Selection, Try Again: " version
+    fi
+done
---
+++ macOS/vGraham.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Setting up vGraham enviroment..."
+echo "Downloading iTerm..."
+#
+
+[ `uname -s` != "Darwin" ] && return
+
+function iterm () {
+    osascript &>/dev/null <<EOF
+    tell application "iTerm2"
+        activate
+        set term to (make new terminal)
+            tell term
+                launch session "Default Session"
+                tell the last session
+                    delay 1
+#                    write text "./vGraham_iTerm.sh"
+                    write text "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vGraham_iTerm.sh)\""
+                end
+            end tell
+#            tell current session of term
+#                delay 1
+#                write text "./vGraham_iTerm.sh"
+#            end tell
+    end tell
+EOF
+}
+iterm $@
+
+echo "Opening iTerm..."
