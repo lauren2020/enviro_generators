28abb88b6d9682f773e578f2c65b1713dd1378b8
Lauren Shultz <lshultz2020@gmail.com>
2019-08-23 18:57:37 -0500

    test proper format2

--- macOS/install.sh
+++ macOS/install.sh
@@ -11,23 +11,21 @@ What version enviroment version would you like to setup?
 read -p "Selection: " version
 
 while [  $accepted -eq 0 ]; do
+    accepted=1
     if [ $version -eq 0 ]
     then
-        accepted=1
-        $(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vBasic.sh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vBasic.sh)"
     elif [ $version -eq 1 ]
     then
-        accepted=1
-        sh -c $(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vGraham.sh)
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vGraham.sh)"
     elif [ $version -eq 2 ]
     then
-        accepted=1
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vFun.sh)"
     elif [ $version -eq 3 ]
     then
-        accepted=1
         echo "Goodbye!"
     else
+        accepted=0
         read -p "Invalid Selection, Try Again: " version
     fi
 done
