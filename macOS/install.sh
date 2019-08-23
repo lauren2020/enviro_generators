e7e2e5bb363a837ec075e379454c0bc3a054fd9a
Lauren Shultz <lshultz2020@gmail.com>
2019-08-23 18:54:14 -0500

    removed unused comments

--- macOS/install.sh
+++ macOS/install.sh
@@ -14,17 +14,14 @@ while [  $accepted -eq 0 ]; do
     if [ $version -eq 0 ]
     then
         accepted=1
-#        ./vBasic.sh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vBasic.sh)"
     elif [ $version -eq 1 ]
     then
         accepted=1
-#        ./vGraham.sh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vGraham.sh)"
     elif [ $version -eq 2 ]
     then
         accepted=1
-#       ./vFun.sh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/lauren2020/enviro_generators/master/macOS/vFun.sh)"
     elif [ $version -eq 3 ]
     then
